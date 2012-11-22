12186f54a8ff2306721d8ec2afa1dffa23ceb2b9
eli levi <2468lelevi@gmail.com>
2012-11-22 15:37:29 +0200

    Revert "Revert "first GUI""
    This reverts commit 671c8b771ee26b79bb976da5d6708ce32f3307c5.

---
+++ Frequent_Buyer/src/com/example/frequent_buyer/LoadingActivity.java
@@ -0,0 +1,40 @@
+package com.example.frequent_buyer;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoadingActivity extends Activity
+{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_loading);
+
+		Thread timer = new Thread()
+		{
+
+			public void run()
+			{
+				try
+				{
+					sleep(3000);
+					Intent toOpen = new Intent(LoadingActivity.this, Login.class); 
+					startActivity(toOpen);
+					finish();
+				}
+				catch(InterruptedException e)
+				{
+
+				}
+			}
+
+		};
+		timer.start();
+	}
+
+
+}
