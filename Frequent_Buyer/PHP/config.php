d475fd2c3874cc61f802ec2963fb4b7281c0f756
yoav fuchs <yoav.fuchs@gmail.com>
2012-11-22 15:30:33 +0200

    first GUI

---
+++ Frequent_Buyer/PHP/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "yoavfu");
+define("DB_PASSWORD", "13834");
+define("DB_DATABASE", "yoavfu");
+?>
---
+++ Frequent_Buyer/gen/com/example/frequent_buyer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.frequent_buyer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
