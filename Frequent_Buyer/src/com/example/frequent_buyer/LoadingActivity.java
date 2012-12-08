b84e31b6c43f22e5b2e6a3f14671a299931d5575
eli levi <2468lelevi@gmail.com>
2012-12-08 22:34:53 +0200

    I want to use in settings to save pass and username more than SQLlite, so i upload several method that need to merge tham.

--- Frequent_Buyer/src/com/example/frequent_buyer/LoadingActivity.java
+++ Frequent_Buyer/src/com/example/frequent_buyer/LoadingActivity.java
@@ -3,30 +3,26 @@ package com.example.frequent_buyer;
 
 import java.util.HashMap;
 
+import com.sender.StatPram;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class LoadingActivity extends Activity
 {
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_loading);
-
-
-
 		Thread timer = new Thread()
 		{
-
 			public void run()
 			{
 				try
 				{
 					sleep(1000);
-
 					/* 
 					 * if the user details are saved(already logged in before) then
 					 * automatically login
@@ -43,7 +39,6 @@ public class LoadingActivity extends Activity
 							activity = new Intent(LoadingActivity.this, BusinessMenu.class);
 						else
 							activity = new Intent(LoadingActivity.this, OwnerMenu.class);
-
 						// Open the activity
 						startActivity(activity);
 						// Close Loading Screen
@@ -65,6 +60,63 @@ public class LoadingActivity extends Activity
 		};
 		timer.start();
 	}
+	/*
+	 * 	need to change to someting like that is more simple and quick from sqllite 
+	 * 
+	 *  	
+	 *  private void saveUser(){
+
+		Boolean exists = false;
+		
+		//check if user already exists
+		for (int i = 0; i < StatPram.numOfUsers; i++){
+			if( settings.getString("user"+i, "false").compareTo(mailText) == 0)
+				exists  = true;
+		}
+		
+		//if not exists then add to list
+		if (!exists){
+			editor.putString("user"+StatPram.numOfUsers,mailText);
+			StatPram.numOfUsers++;
+			editor.putInt("numOfUsers", StatPram.numOfUsers);
+
+			// Commit the edits!
+			editor.commit();
+		}
+	}
 
 
+	//gets the email and password from edit texts  
+	private void getText(){
+		passText = pass.getText().toString();
+		mailText = mail.getText().toString();
+	}
+
+
+	//saves mail and password if checkbox is true
+	private void savePref()
+	{
+		editor.putString("mail", mailText);
+		editor.putString("pass", passText);
+
+		// Commit the edits!
+		editor.commit();
+	}
+
+
+	//last user that saved his mail
+	private void setSavedUser()
+	{
+		mailPref = settings.getString("mail", "false");
+		passPref = settings.getString("pass", "false");
+		if (mailPref.compareTo("false")!=0 && passPref.compareTo("false")!=0){
+			mail.setText(mailPref);
+			pass.setText(passPref);
+		}
+	}
+
+	 *  
+	 *  
+	 *  
+	 *  */
 }
