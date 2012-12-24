c1768a11d64d29ccef9a5161b6b98bc5f806794b
eli levi <2468lelevi@gmail.com>
2012-12-24 18:26:26 +0200

    green - signup first working, again rejected (o.k)

--- Frequent_Buyer_unit_tests/src/UnitTests.java
+++ Frequent_Buyer_unit_tests/src/UnitTests.java
@@ -8,6 +8,7 @@ public class UnitTests {
 	@Test
 	public void tests() {
 		Data_base_mock d = new Data_base_mock();
+		assertEquals(true, d.SignUp("yuval@gmail.com", "pass"));
 		assertEquals(false, d.SignUp("yuval@gmail.com", "pass"));
 	}
 	
