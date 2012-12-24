35656910929bafbb83b9a0fe0304c66708fecdce
eli levi <2468lelevi@gmail.com>
2012-12-24 17:56:17 +0200

    red -cant sign, username format invalid.(o.k)

--- Frequent_Buyer_unit_tests/src/UnitTests.java
+++ Frequent_Buyer_unit_tests/src/UnitTests.java
@@ -8,8 +8,7 @@ public class UnitTests {
 	@Test
 	public void tests() {
 		Data_base_mock d = new Data_base_mock();
-		assertEquals(true, d.SignUp("eli@gmail.com", "pass"));
-		assertEquals(true, d.Login("moshe@gmail.com", "pass"));
+		assertEquals(true, d.SignUp("gmail.com", "pass"));
 	}
 	
 
