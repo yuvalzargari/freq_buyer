67cffecab1bf77d79023092cae442c9b752d0c8e
eli levi <2468lelevi@gmail.com>
2012-12-24 18:33:50 +0200

    green - Register multiple users, possible (o.k)

--- Frequent_Buyer_unit_tests/src/UnitTests.java
+++ Frequent_Buyer_unit_tests/src/UnitTests.java
@@ -8,8 +8,12 @@ public class UnitTests {
 	@Test
 	public void tests() {
 		Data_base_mock d = new Data_base_mock();
-		assertEquals(false, d.Login("yuval@gmail.com", "pass"));
-		
+		assertEquals(true, d.SignUp("yuval@gmail.com", "pass"));
+		assertEquals(true, d.SignUp("eli@gmail.com", "pass"));
+		assertEquals(true, d.SignUp("yuval2@gmail.com", "pass"));
+		assertEquals(true, d.SignUp("eli2@gmail.com", "pass"));
+		assertEquals(true, d.SignUp("yuval3@gmail.com", "pass"));
+		assertEquals(true, d.SignUp("eli3@gmail.com", "pass"));
 	}
 	
 
