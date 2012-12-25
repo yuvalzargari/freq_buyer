1ce4870935f45f44407280d5e2cba0506ee17fbc
eli levi <2468lelevi@gmail.com>
2012-12-25 21:21:55 +0200

    up

---
+++ Frequent_Buyer_unit_tests/src/Data_base_mock.java
@@ -0,0 +1,89 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Data_base_mock 
+{
+	String db [][];
+	
+	public Data_base_mock() {
+		db = new String [10][5];
+		db[0][0] = "id";
+		db[0][1] = "email";
+		db[0][2] = "type";
+		db[0][3] = "password";
+		db[0][4] = "numToCoupon";
+		
+		db[1][0] = "0";
+		db[1][1] = "eitan@test.com";
+		db[1][2] = "owner";
+		db[1][3] = "1234";
+		db[1][4] = "100";
+		
+		//System.out.println(toString());
+	}
+	public boolean SignUp(String email,String pass) {
+		if (!is_email_valid(email)) {
+			return false;
+		}
+		for (int i = 2; i < db.length ; i++) 
+		{
+			if (email == db[i][1] && pass == db[i][3]) {
+				return false;
+			}
+		}
+		int temp;
+		for (int i = 2; i < db.length; i++) {
+			if (db[i][0] == null) 
+			{
+				temp = Integer.parseInt( db[i-1][0]);
+				db[i][0] = temp+1 +"";
+				db[i][1] = email;
+				db[i][2] = "client";
+				db[i][3] = pass;
+				db[i][4] = "10";
+				break;
+			}
+		}
+		return true;
+	}
+	//checks if mail is valid
+	private boolean is_email_valid(String hex) 
+	{
+		Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+		Matcher m = p.matcher(hex);
+		boolean matchFound = m.matches();
+		return matchFound;
+	}
+	
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < db.length; i++) 
+		{
+			for (int j = 0; j < db[0].length; j++) 
+			{
+				s+=db[i][j]+"\t";
+				if(j == 1 && i!=1 && i!=2)
+					s+="\t";
+				if(j == 3 && i != 0)
+					s+="\t";
+				if (j==1&& db[i][j] != null) {
+
+				}	
+			}
+			s+="\n";
+		}
+		return s;
+	}
+	public boolean Login(String email,String pass) 
+	{
+		for (int i = 1; i < db.length ; i++) 
+		{
+			if (email == db[i][1] && pass == db[i][3]) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+
--- Frequent_Buyer_unit_tests/src/UnitTests.java
+++ Frequent_Buyer_unit_tests/src/UnitTests.java
@@ -9,11 +9,9 @@ public class UnitTests {
 	public void tests() {
 		Data_base_mock d = new Data_base_mock();
 		assertEquals(true, d.SignUp("yuval@gmail.com", "pass"));
-		assertEquals(true, d.SignUp("eli@gmail.com", "pass"));
-		assertEquals(true, d.SignUp("yuval2@gmail.com", "pass"));
-		assertEquals(true, d.SignUp("eli2@gmail.com", "pass"));
-		assertEquals(true, d.SignUp("yuval3@gmail.com", "pass"));
-		assertEquals(true, d.SignUp("eli3@gmail.com", "pass"));
+		assertEquals(true, d.Login("yuval@gmail.com", "pass"));
+		assertEquals(true, d.Login("yuval@gmail.com", "pass"));
+		
 	}
 	
 
