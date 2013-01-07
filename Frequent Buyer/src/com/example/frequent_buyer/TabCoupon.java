263316e3051906bdd4b151bab2a39ee3182bc51b
eli levi <2468lelevi@gmail.com>
2013-01-07 16:50:11 +0200

    get coupon number and benefit working
    i added the col of benefit to table in db,
    if its not working you need ti access with elile acount or add coupon
    and benefit to table in db.
    i change the PHP script getcoupon and do inneer join.

--- Frequent Buyer/src/com/example/frequent_buyer/TabCoupon.java
+++ Frequent Buyer/src/com/example/frequent_buyer/TabCoupon.java
@@ -4,12 +4,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class TabCoupon extends Activity 
@@ -23,6 +20,7 @@ public class TabCoupon extends Activity
 	String business = staticParams.businessName;
 	String benefit_name;
 	int num_lest_to_benefit;
+	ProgressDialog WaitView;
 
 	public void onCreate(Bundle savedInstanceState) 
 	{
@@ -30,10 +28,8 @@ public class TabCoupon extends Activity
 		setContentView(R.layout.activity_coupon);
 		number_lest_to_benefit = (TextView)findViewById(R.id.number_to_coupon_id);
 		text_of_benefit = (TextView)findViewById(R.id.benefit_text_id);
-		//Toast.makeText(this, business, Toast.LENGTH_LONG).show(); 
-		// get from server
 		
-
+		// get from server
 		get_number_lest_to_benefit(email, business);
 
 		staticParams.userCounterToBenefit = num_lest_to_benefit;
@@ -45,6 +41,8 @@ public class TabCoupon extends Activity
 	private void get_number_lest_to_benefit(String user_email , String business_selected)
 	{
 		final BusinessUsersFunction UsersFunction = new BusinessUsersFunction();
+		//WaitView = ProgressDialog.show(TabCoupon.this, "", "Loading...");
+		//WaitView.show();
 		Thread toRun = new Thread()
 		{
 			public void run()
@@ -53,13 +51,14 @@ public class TabCoupon extends Activity
 			}
 		};toRun.start();
 		try {
+			
 			toRun.join();
 		} catch (InterruptedException e) {}
-		showJson(backFromServer);
-
+		//WaitView.dismiss();
 		try {
-			benefit_name = "hamburger" ;
+			benefit_name = 		backFromServer.getString("Benefit") ;
 			num_lest_to_benefit = backFromServer.getInt("coupon");
+			// if it return false or junk there is no line with username in business_users
 		} catch (JSONException e) {}
 	}
 	
@@ -71,18 +70,19 @@ public class TabCoupon extends Activity
 		text_of_benefit.setText(benefit_name);		
 	}
 
-	private void showJson(JSONObject backFromServer) {
-		AlertDialog.Builder alertbox2 = new AlertDialog.Builder(this);
-		alertbox2.setTitle("jeson");
-		try {
-			alertbox2.setMessage(backFromServer.toString(1));
-		} catch (JSONException e) {alertbox2.setMessage(e.toString());}
-		alertbox2.setPositiveButton("ok", new DialogInterface.OnClickListener()
-		{
-			public void onClick(DialogInterface arg0, int arg1) {}
-		});
-		alertbox2.show();		
-	}
+//	private void showJson(JSONObject backFromServer) 
+//	{
+//		AlertDialog.Builder alertbox2 = new AlertDialog.Builder(this);
+//		alertbox2.setTitle("jeson");
+//		try {
+//			alertbox2.setMessage(backFromServer.toString(1));
+//		} catch (JSONException e) {alertbox2.setMessage(e.toString());}
+//		alertbox2.setPositiveButton("ok", new DialogInterface.OnClickListener()
+//		{
+//			public void onClick(DialogInterface arg0, int arg1) {}
+//		});
+//		alertbox2.show();		
+//	}
 
 }
 
--- PHP Scripts - Server/getCoupon.php
+++ PHP Scripts - Server/getCoupon.php
@@ -11,47 +11,36 @@
  */
 if (isset($_POST['tag']) && $_POST['tag'] != '') 
 {
-    // get tag
-    $tag = $_POST['tag'];
- 
-    // include db handler
-    require_once '/home/a9208348/public_html/DB_Users_Functions.php';
-    $db_users = new DB_Users_Functions();
-    require_once '/home/a9208348/public_html/DB_Business_Functions.php';
-    $db_business = new DB_Business_Functions();
-    require_once '/home/a9208348/public_html/DB_BusinessUsers_Functions.php';
-    $db_business_users = new DB_BusinessUsers_Functions();
- 
-    // response Array
-    $response = array("tag" => $tag, "success" => 0, "error" => 0);
- 
+	require_once '/home/a9208348/public_html/DB_Connect.php';
+        // connecting to database
+        $db = new DB_Connect();
+        $db->connect();
 
-    // Request type is check Login
-    $email = $_POST['email'];
-    $business_name = $_POST['businessName'];
-    
-    $user = $db_users->getUserIDByEmail($email);
-    $userID = $user["uid"];
-    
-    $business = $db_business->getBusinessIDByName($business_name);
-    $businessID = $business["id"];
-    
-    $coupon = $db_business_users->getcouponByUserIDAndBusinessID($userID, $businessID);
- 
-    if ($coupon != false) 
-	{
-        // business user found
-		$response["success"] = 1;
-        $response["business_user"]["coupon"] = $coupon["coupon"];
-        echo json_encode($response);
-    } 
-	else 
-	{
-		// business user not found
-        // echo json with error = 1
-        $response["error"] = 1;
-		echo json_encode($response);
-    }
+    	$email = $_POST['email'];
+   	$business_name = $_POST['businessName'];	    
+        
+    	$result = mysql_query(
+				"SELECT bu.coupon, bu.Benefit
+				 FROM business_users bu
+				 INNER JOIN users u
+  				 ON u.uid = bu.user_id AND u.email = '$email'
+				 INNER JOIN business b
+				 ON b.id = bu.bussines_id 
+				 WHERE b.`business name` = '$business_name'"
+			) ;
+
+	// check for result   
+	$no_of_rows = mysql_num_rows($result);     
+	if ($no_of_rows > 0) 	
+	{           
+		$result = mysql_fetch_array($result);	
+		echo json_encode($result);    
+	}        
+	else 	
+	{         
+		// business user not found        
+		return false;       
+	}
 } 
 else 
 {
