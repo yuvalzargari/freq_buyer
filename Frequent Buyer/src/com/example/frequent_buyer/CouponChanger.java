9f322e0960804beef37d6ac9513a88811179d3f6
eli levi <2468lelevi@gmail.com>
2013-01-10 20:34:02 +0200

    commit

--- Frequent Buyer/src/com/example/frequent_buyer/CouponChanger.java
+++ Frequent Buyer/src/com/example/frequent_buyer/CouponChanger.java
@@ -1,21 +1,29 @@
 package com.example.frequent_buyer;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpConnectionParams;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,22 +33,22 @@ public class CouponChanger extends Activity
 {
 
 	EditText newNumToCopon,newBenefit;
-	Button setCopon;
+	Button setCopon,subtract ;
 	Editable NumToCopon;
 	String Benefit, email, businessname;
-	List<NameValuePair> params;
-	private JSONParser jsonParser;
-	private String url = "http://eliproj1.site88.net/setCuopon.php";
+
+	protected CharSequence result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.coponchanger);
-		jsonParser = new JSONParser();
 		newBenefit = (EditText)findViewById(R.id.setbenefitcoponchangerID);
 		newNumToCopon = (EditText)findViewById(R.id.setnumcoponchangerID);
 		setCopon = (Button)findViewById(R.id.setcoponbuttunID);
+		subtract = (Button)findViewById(R.id.subtractButtunID);
+
 
 		setCopon.setOnClickListener(new View.OnClickListener()
 		{
@@ -53,41 +61,49 @@ public class CouponChanger extends Activity
 				setDataonserver();
 			}
 		});
+
+		subtract.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View v) {
+				subTractOne();
+			}
+
+		});
+	}
+	private void subTractOne() {
+		List<NameValuePair> params;
+		String url = "http://eliproj1.site88.net/public_html/subtractCuopon.php";
+		params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("subtractCuopon", "setCoupon"));
+		params.add(new BasicNameValuePair("email", email));	
+		sendAndResive(url,params);
 	}
 	private void setDataonserver() 
 	{
+		List<NameValuePair> params;
+		String url = "http://eliproj1.site88.net/public_html/setCuopon.php";
 		params = new ArrayList<NameValuePair>();
 		params.add(new BasicNameValuePair("tag", "setCoupon"));
 		params.add(new BasicNameValuePair("email", email));
 		params.add(new BasicNameValuePair("businessName", businessname));
 		params.add(new BasicNameValuePair("setCoupon", NumToCopon.toString()));
 		params.add(new BasicNameValuePair("benefit", Benefit));
-		
 		Toast.makeText(getApplicationContext(), email+" "+businessname+" "+NumToCopon.toString()+" "+Benefit, Toast.LENGTH_LONG).show();
-		
-		sendAndResive();
+		sendAndResive(url,params);
 	}
 
-	private void sendAndResive()
+	private void sendAndResive(final String url, final List<NameValuePair> params) 
 	{
-		Runnable runnable = new Runnable() 
+		try
 		{
-			public void run() {
-				try {
-					//jsonParser.getJSONFromUrl(url, params);
-					HttpClient httpClient = new DefaultHttpClient();
-					HttpConnectionParams.setConnectionTimeout(httpClient.getParams(), 5000);
-					HttpPost httpPost = new HttpPost(url);
-					httpPost.setEntity(new UrlEncodedFormEntity(params));
-
-					HttpResponse httpResponse = httpClient.execute(httpPost);
-					//HttpEntity httpEntity = httpResponse.getEntity();
-				} catch (Exception e) {}
-			}
-		};
-		Thread mythread = new Thread(runnable);
-		mythread.start();
+			HttpPost httpPost = new HttpPost(url);
+			HttpClient httpClient = new DefaultHttpClient();
+			httpPost.setEntity(new UrlEncodedFormEntity(params));
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
 	}
-
-
 }
